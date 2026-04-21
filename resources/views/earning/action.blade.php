ec114121991c0f965e6ae885b5affc008dd497e5
Ahmed M Hamzah <71948774+ahmed-coding@users.noreply.github.com>
2026-04-21 03:34:15 +0300

    Refactor form fields in provider payout creation: comment out unused payment method and bank selection fields for improved clarity and maintainability

--- resources/views/earning/action.blade.php
+++ resources/views/earning/action.blade.php
@@ -5,8 +5,8 @@ $auth_user= authSession();
 {{-- {{ Form::open(['route' => ['earning.destroy', $row->id], 'method' => 'delete','data--submit'=>'earning'.$row->id]) }}
 <div class="d-flex justify-content-end align-items-center">
 @if(auth()->user()->hasAnyRole(['admin']))
-    <a class="me-3" href="{{ route('earning.destroy', $row->id) }}" data--submit="earning{{$row->id}}" 
-        data--confirmation='true' 
+    <a class="me-3" href="{{ route('earning.destroy', $row->id) }}" data--submit="earning{{$row->id}}"
+        data--confirmation='true'
         data--ajax="true"
         data-datatable="reload"
         data-title="{{ __('messages.delete_form_title',['form'=>  __('messages.earning') ]) }}"
